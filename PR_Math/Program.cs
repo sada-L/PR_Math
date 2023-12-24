14e59991b2d60d425226597a7a1838ed778b8afc
sada <g-g2g-g78@mail.ru>
2023-12-24 19:14:30 +0300

    Initial commit

---
+++ PR_Math/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program {
+    public static void Main(string[] args) {
+         int i = 0;
+         Console.WriteLine("Введите a, b, e");
+         string[] str = Console.ReadLine().Split();
+         double a = Convert.ToDouble(str[0]);
+         double b = Convert.ToDouble(str[1]);
+         double e = Convert.ToDouble(str[2]);
+         double x = 0;
+         double d = 0;
+         while (true) {
+             double c = (a + b) / 2;
+             if (Math.Sin(a * 2) - Math.Log(Math.E, a)
+                 * (Math.Sin(c * 2) - Math.Log(Math.E, c)) < 0)
+                 b = c;
+             else
+                 a = c;
+             if (b - a < e) {
+                 x = (a + b) / 2;
+                 d = (b - a) / 2;
+             }
+             i++;
+             Console.WriteLine($"N: {i}  x:{x},b:{d}");
+             Console.ReadLine();
+         }
+    }
+}
+
